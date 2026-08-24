c3217cbc99b6017eaa32297022652f5521e9ff93
Francisco Kirhman <francisco.osorio@ug.uchile.cl>
2026-08-24 09:08:53 -0400

    PWA instalable en iOS, pestana Fuerza con 1RM estimado y selector de curvas, heatmap de constancia y marcado de superseries

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Cachea el tablero para que funcione sin señal en el gimnasio.
+// Estrategia: red primero (para ver siempre el plan actualizado), cache como respaldo.
+const CACHE = 'entreno-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
